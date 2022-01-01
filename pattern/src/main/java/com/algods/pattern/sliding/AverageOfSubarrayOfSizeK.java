659c09215c84df53ce24c7a4cbbe3de6933b57d9
tarini <tarininegi@gmail.com>
2022-01-01 22:31:14 +0530

    Average Sum sliding window

---
+++ pattern/src/main/java/com/algods/pattern/sliding/AverageOfSubarrayOfSizeK.java
@@ -0,0 +1,32 @@
+package com.algods.pattern.sliding;
+
+import java.util.Arrays;
+
+public class AverageOfSubarrayOfSizeK {
+
+	public static double[] findAverage(int K, int[] arr) {
+		
+		double[] result = new double[arr.length - K + 1];
+		int windowStart = 0, windowSum = 0;
+		
+		for(int windowEnd=0; windowEnd < arr.length; windowEnd++) {
+			
+			windowSum += arr[windowEnd];
+			
+			if(windowEnd >= K-1) {
+				result[windowStart] = windowSum;
+				windowSum -= arr[windowStart];
+				windowStart++;
+			}
+		}
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] arr = new int[] {1, 3, 2, 6, -1, 4, 1, 8, 2};
+		double[] result = findAverage(5, arr);
+		System.out.println(Arrays.toString(result));
+	}
+}
